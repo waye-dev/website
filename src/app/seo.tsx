2fa6956c1a6d2323ad09e77893ccf78a5e5dc5e7
IgboPharaoh <solomoneze62@gmail.com>
2025-02-03 16:52:17 +0100

    fix url path

--- src/app/seo.tsx
+++ src/app/seo.tsx
@@ -14,16 +14,16 @@ export function genPageMetadata({ title, description, image, url, ...rest }: Pag
     openGraph: {
       title: `${title}`,
       description: description || "Waye empowers builders to stay clear-headed, motivated, and aligned for long-term impact.",
-      url: url || "https://waye.dev",
+      url: url || "https://www.waye.dev",
       siteName: "Waye",
-      images: image ? [image] : [{ url: "https://waye.dev/images/webclip.webp" }],
+      images: image ? [image] : [{ url: "https://www.waye.dev/images/webclip.webp" }],
       locale: "en_US",
       type: "website",
     },
     twitter: {
       title: `${title}`,
       card: "summary_large_image",
-      images: image ? [image] : ["https://waye.dev/images/webclip.webp"],
+      images: image ? [image] : ["https://www.waye.dev/images/webclip.webp"],
     },
     ...rest,
   };
