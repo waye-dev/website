7e1cb66d73ee23b74eb37aa7b60f69a369c40517
IgboPharaoh <solomoneze62@gmail.com>
2025-02-11 05:43:58 +0100

    update app data and create subscribe route

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,13 @@
 export const FORM_ID = process.env.NEXT_PUBLIC_JOTFORM_FORM_ID ?? "";
+
 export const API_KEY = process.env.NEXT_PUBLIC_JOTFORM_API_KEY ?? "";
+
+export const MAILCHIMP_API_KEY = process.env.MAILCHIMP_API_KEY ?? "";
+
+export const MAILCHIMP_SERVER = process.env.MAILCHIMP_SERVER ?? "";
+
+export const MAILCHIMP_LIST_ID = process.env.MAILCHIMP_LIST_ID ?? "";
+
+if (!MAILCHIMP_API_KEY) {
+  throw new Error("MAILCHIMP_API_KEY not found");
+}
