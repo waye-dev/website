e7b529c678bd88908aef24b2503505a3a0455704
IgboPharaoh <solomoneze62@gmail.com>
2025-09-22 07:33:57 +0100

    add submit donation details action

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,4 @@
-export const isValidEmail = (email: string): boolean => {
-  const emailRegex = /^(?=[a-zA-Z0-9@._%+-]{6,254}$)[a-zA-Z0-9._%+-]{1,64}@(?:[a-zA-Z0-9-]{1,63}\.){1,8}[a-zA-Z]{2,63}$/;
-  return emailRegex.test(email);
-};
+export const isValidEmail = (email: string): boolean => EMAIL_REGEX.test(email);
 
 export const isValidName = (name: string): boolean => {
   const nameRegex = /^[a-zA-Z\s'-]{2,50}$/;
@@ -26,3 +23,5 @@ export const checkFormValues = (name: string, email: string, setMessage: React.D
 
   setMessage("");
 };
+
+export const EMAIL_REGEX = /^(?=[a-zA-Z0-9@._%+-]{6,254}$)[a-zA-Z0-9._%+-]{1,64}@(?:[a-zA-Z0-9-]{1,63}\.){1,8}[a-zA-Z]{2,63}$/;
