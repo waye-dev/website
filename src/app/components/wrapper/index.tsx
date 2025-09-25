c3a5761d1a6aebab59729f8b120c1a675f799f45
IgboPharaoh <solomoneze62@gmail.com>
2025-09-25 11:48:51 +0100

    add layout font

--- src/app/components/wrapper/index.tsx
+++ src/app/components/wrapper/index.tsx
@@ -1,5 +1,6 @@
+import { cn } from "@/utils";
 import React from "react";
 
 export default function Wrapper({ className, ...props }: React.ComponentPropsWithoutRef<"div">) {
-  return <div className={`w-full max-w-[1400px] px-4 xl:px-8 mx-auto ${className}`} {...props} />;
+  return <div className={cn("w-full max-w-[1400px] px-4 xl:px-8 mx-auto", className)} {...props} />;
 }
