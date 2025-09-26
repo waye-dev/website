1684be2cc067d201030f4748d0b3b0eacf2d589f
IgboPharaoh <solomoneze62@gmail.com>
2025-09-26 22:37:12 +0100

    update mail chimp error handling

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,7 +7,3 @@ export const MAILCHIMP_API_KEY = process.env.NEXT_PUBLIC_MAILCHIMP_API ?? "";
 export const MAILCHIMP_SERVER = process.env.NEXT_PUBLIC_MAILCHIMP_SERVER ?? "";
 
 export const MAILCHIMP_LIST_ID = process.env.NEXT_PUBLIC_MAILCHIMP_LIST_ID ?? "";
-
-if (!MAILCHIMP_API_KEY) {
-  throw new Error("MAILCHIMP_API_KEY not found");
-}
