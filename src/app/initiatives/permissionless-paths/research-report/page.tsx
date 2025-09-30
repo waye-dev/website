e05ade96be26023949189ea32c97b5ce6b5cf2b9
ttarinov <62486254+ttarinov@users.noreply.github.com>
2025-09-30 07:58:25 -0300

    Strategies wall animation (#67)
    * zoom-in & zoom-out and paralax-sections effect
    
    * sections paralax scroll with overflow
    
    * improuving overlap
    
    * overflow smoothness + enter window position adjustment
    
    * right position of the exit
    
    * mobile vertion
    
    * auto calculated zommed-in size for different screen sizes
    
    * removing from the page the old vertion
    
    * reusable strategy-sub-section
    
    * removed import

--- src/app/initiatives/permissionless-paths/research-report/page.tsx
+++ src/app/initiatives/permissionless-paths/research-report/page.tsx
@@ -1,18 +1,17 @@
 "use client";
 
 import { useState } from "react";
-import Image from "next/image";
 import Wrapper from "@/app/components/wrapper";
 import { StudyOverviewSection } from "@/app/components/research-report/study-overview-section";
 import { GLOSSARY_LIST, GLOSSARY_TEXT_SECTIONS, GlossaryChart, GlossarySection } from "@/app/components/research-report/glossary-section";
 import { TopLevelAnalysis } from "@/app/components/research-report/top-level-analysis";
 import { TyrannyOfPermissionlessness } from "@/app/components/research-report/tyranny-of-permissionlessness";
 import { Strategies } from "@/app/components/research-report/strategies";
+import { StrategiesWall } from "@/app/components/research-report/strategies-wall";
 import Eyeballs from "@/app/components/research-report/_components/eyeballs";
 import { BeyondFinancialSustainability } from "@/app/components/research-report/beyond-financial-sustainability";
 import { RecommendationReveal } from "@/app/components/research-report/recommendation-reveal";
 import { Conclusion } from "@/app/components/research-report/conclusion";
-import { StrategySubSections } from "@/app/components/research-report/strategy-sub-sections";
 
 export default function ResearchReport() {
   const [activeId, setActiveId] = useState<number | null>(null);
@@ -140,21 +139,10 @@ export default function ResearchReport() {
         <Wrapper className='max-w-[1000px] xl:max-w-[1250px] py-24'>
           <Strategies />
         </Wrapper>
-
-        <div className='w-full max-h-[600px bg-cover bg-center bg-no-repeat relative' style={{ backgroundImage: "url('/svgs/block-bg.svg')" }}>
-          <div className='grid grid-cols-2 gap-4 md:gap-8 items-center justify-items-center p-4 md:p-8 h-full'>
-            {strategyImages.map((image) => (
-              <div key={image.alt} className='flex items-center justify-center'>
-                <Image src={image.src} alt={image.alt} width={image.width} height={image.height} className='object-contain' />
-              </div>
-            ))}
-          </div>
-        </div>
       </div>
-      <div className='bg-gray-custom-400 text-black'>
-        <Wrapper className='max-w-[1000px] xl:max-w-[1250px] py-24'>
-          <StrategySubSections />
-        </Wrapper>
+      
+      <div className="bg-blue-custom-1200">
+        <StrategiesWall />
       </div>
       <div className='bg-blue-custom-1100'>
         <Wrapper className='max-w-[1000px] xl:max-w-[1250px] py-24'>
