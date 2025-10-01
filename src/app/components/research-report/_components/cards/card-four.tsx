0e5166bb0367ca13884421e7cee24b543d192d3d
Bland Irony <blandirony@gmail.com>
2025-10-01 15:59:01 +0100

    :sparkles: Fixed card style issue.

--- src/app/components/research-report/_components/cards/card-four.tsx
+++ src/app/components/research-report/_components/cards/card-four.tsx
@@ -19,7 +19,7 @@ export const CardFour = ({ isExpanded, onLabelClick, onScroll, position, isInter
         }`}>
             <div className="relative h-full w-full">
                 <span className="relative top-0 left-[240px] cursor-pointer pointer-events-auto" style={{ left: leftPosition }} onClick={onLabelClick}>
-                    <svg className={`${isInteracted ? "w-[12rem] md:w-[32rem]" : "w-[12rem] md:23rem"}`} viewBox="0 0 399 55" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <svg className={`${isInteracted ? "w-[12rem] md:w-[32rem]" : "w-[12rem] md:w-[23rem]"}`} viewBox="0 0 399 55" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path id="Rectangle 18" d="M19.0525 14.5368C22.1981 5.81401 30.4753 0 39.7479 0H346.675C355.06 0 362.717 4.76689 366.417 12.2917L371.323 22.2691C377.411 34.648 386.673 45.1907 398.164 52.8219C398.164 52.8219 3.13892e-06 57.4827 1.56946e-06 52.8219C-1.85806e-06 42.6434 11.2175 36.2632 14.6704 26.6882L19.0525 14.5368Z" fill="#c4def8"/>
                         <text className="inline-block text-[1.25rem] font-inknutAntiqua" x="50%" y="54%" textAnchor="middle" dominantBaseline="middle" fill="#222222" fontSize="16">
                             Institutional Factors
