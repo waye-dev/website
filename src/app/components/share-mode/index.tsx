cb7eab43e3b0ec369627d9f5cd94b9634c93f131
ttarinov <62486254+ttarinov@users.noreply.github.com>
2025-10-11 22:40:35 -0300

    remove floating button

--- src/app/components/share-mode/index.tsx
+++ src/app/components/share-mode/index.tsx
@@ -1,6 +1,5 @@
 export { SharePopover } from "./share-popover";
 export { ShareableContent } from "./shareable-content";
-export { FloatingShareButton } from "./floating-share-button";
 export { ShareButtons } from "./share-buttons";
 export { NostrModal } from "./nostr-modal";
 export { ShareModeProvider, useShareMode } from "@/contexts/share-mode-context";
