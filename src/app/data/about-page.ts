12fc6ef9b6f865fcd842570b0bee4240301efff1
IgboPharaoh <solomoneze62@gmail.com>
2025-10-21 16:25:29 +0100

    update about page data

---
+++ src/app/data/about-page.ts
@@ -0,0 +1,128 @@
+export const ABOUT_PAGE_DATA = {
+  maintainers: [
+    {
+      name: "Anna loren",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: "/svgs/about-page/anna-loren.svg",
+      position: "left",
+    },
+    {
+      name: "Amithi Fox",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: "/svgs/about-page/amithi-fox.svg",
+      position: "left",
+    },
+  ],
+  boardOfDirectors: [
+    {
+      name: "Neha Bing",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: "/svgs/about-page/neha-bing.svg",
+      position: "right",
+    },
+    {
+      name: "Pablo Gellar",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: "/svgs/about-page/pablo-gellar.svg",
+      position: "left",
+    },
+  ],
+  designers: [
+    {
+      name: "Stacey Grik",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: "/svgs/about-page/stacey-grik.svg",
+      position: "left",
+    },
+    {
+      name: "Griff Hall",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: "/svgs/about-page/griff-hall.svg",
+      position: "left",
+    },
+    {
+      name: "Magali Mendes",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: "/svgs/about-page/magali-mendes.svg",
+      position: "left",
+    },
+  ],
+  researchers: [
+    {
+      name: "Laura Jacks",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: "/svgs/about-page/laura-jacks.svg",
+      position: "left",
+    },
+    {
+      name: "Nadia Kot",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: "/svgs/about-page/nadia-kot.svg",
+      position: "left",
+    },
+  ],
+  advisorsAndFacilitators: [
+    {
+      name: "Femi Ram",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: "/svgs/about-page/femi-ram.svg",
+      position: "right",
+    },
+    {
+      name: "Aj Kole",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: "/svgs/about-page/aj-kole.svg",
+      position: "right",
+    },
+    {
+      name: "Troy Seva",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: "/svgs/about-page/troy-seva.svg",
+      position: "left",
+    },
+    {
+      name: "Odell Tom",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: "/svgs/about-page/odell-tom.svg",
+      position: "left",
+    },
+    {
+      name: "Vedang Pated",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: "/svgs/about-page/vedang-pated.svg",
+      position: "center",
+    },
+    {
+      name: "Noemi Watts",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: "/svgs/about-page/noemi-watts.svg",
+      position: "center",
+    },
+    {
+      name: "Debbie Morgan",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: "/svgs/about-page/debbie-morgan.svg",
+      position: "center",
+    },
+    {
+      name: "Jessie J",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: "/svgs/about-page/jessie-j.svg",
+      position: "center",
+    },
+  ],
+  sponsors: [
+    {
+      name: "NRF",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: "/svgs/about-page/nrf-image.svg",
+      position: "right",
+    },
+    {
+      name: "OpenSats",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: "/svgs/about-page/opensats-image.svg",
+      position: "right",
+    },
+  ],
+};
