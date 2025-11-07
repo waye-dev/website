fb6d330e0587b2edb415ad26da0c74c8cd6c6d29
ttarinov <62486254+ttarinov@users.noreply.github.com>
2025-11-07 20:40:47 -0300

    bottom line on mobile -crocodile

--- src/app/components/research-report/core-findings-the-tyrany/croc-animation/index.tsx
+++ src/app/components/research-report/core-findings-the-tyrany/croc-animation/index.tsx
@@ -10,7 +10,7 @@ export const CrocAnimation = () => {
     return (
         <div ref={containerRef} className="relative h-[60vh] w-full z-[1] overflow-hidden">
             <BackgroundWoman />
-                <span ref={crocRef} className="absolute -bottom-10 left-0 inline-block will-change-transform z-[1]">
+                <span ref={crocRef} className="absolute -bottom-16 left-0 inline-block will-change-transform z-[1]">
                     <Crocodile />
                 </span>
         </div>
